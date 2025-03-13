a4a5c8d97f69b56bdfcaf9ad1a7264d46577ad0e
aratama <16192627+aratama@users.noreply.github.com>
2025-03-14 00:11:04 +0900

    ga

--- routes/_app.tsx
+++ routes/_app.tsx
@@ -8,6 +8,22 @@ export default function Layout({ Component, data }: PageProps<Data>) {
   return (
     <html lang={currentLang}>
       <head>
+        <script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-7P3DZC7D3N"
+        ></script>
+
+        <script
+          // deno-lint-ignore react-no-danger
+          dangerouslySetInnerHTML={{
+            __html: `
+window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+gtag('config', 'G-7P3DZC7D3N');
+`.trim(),
+          }}
+        ></script>
         <meta charset="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <title>Emoji Frog</title>
